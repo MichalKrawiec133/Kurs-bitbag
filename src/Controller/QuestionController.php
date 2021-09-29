1ed2678f4f4229db5c1625cacbfae8efb229d323
michalkrawiec133 <michalkrawiec133@gmail.com>
2021-09-29 21:11:55 +0200

    /

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,10 +2,8 @@
 
 namespace App\Controller;
 
-use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use App\Service\MarkdownHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
 
 class QuestionController extends AbstractController
@@ -24,7 +22,7 @@ class QuestionController extends AbstractController
     }
 
 
-    public function show($slug, MarkdownParserInterface $markdownParser,CacheInterface $cache)
+    public function show($slug, MarkdownHelper $markdownHelper)
     {
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
@@ -34,11 +32,7 @@ class QuestionController extends AbstractController
 
         $questionText = 'I\'ve been turned into a cat, any *thoughts* on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
 
-        $parsedQuestionText = $cache->get('markdown_'.md5($questionText), function () use($questionText, $markdownParser){
-            return $parsedQuestionText= $markdownParser->transformMarkdown($questionText);
-        });
-
-        dump($cache);
+        $parsedQuestionText = $markdownHelper->parse($questionText);
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
