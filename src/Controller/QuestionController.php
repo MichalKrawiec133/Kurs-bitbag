da35b357722275d21c8e49f61183f69f89a3c5e7
michalkrawiec133 <michalkrawiec133@gmail.com>
2021-10-01 16:39:59 +0200

    /

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,11 +3,22 @@
 namespace App\Controller;
 
 use App\Service\MarkdownHelper;
+use Psr\Log\LoggerInterface;
+use Sentry\State\HubInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
+
+    private $logger;
+    private $isDebug;
+
+    public function __construct(LoggerInterface $logger, bool $isDebug)
+    {
+        $this->logger = $logger;
+        $this->isDebug = $isDebug;
+    }
 
     public function homepage(Environment $twigEnvironment)
     {
@@ -22,8 +33,18 @@ class QuestionController extends AbstractController
     }
 
 
-    public function show($slug, MarkdownHelper $markdownHelper)
+    public function show($slug, MarkdownHelper $markdownHelper, HubInterface $sentryHub)
     {
+
+        dump($sentryHub->getClient());
+
+        //throw new \Exception('Bad stuff happened!');
+
+        if ($this->isDebug){
+            $this->logger->info('We are in debug mode!');
+        }
+
+
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
             'Honestly, I like furry shoes better than MY cat',
