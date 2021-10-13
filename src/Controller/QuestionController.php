4a09aeefa67c3136e8e1ef5da707ad7f445ae3a1
michalkrawiec133 <michalkrawiec133@gmail.com>
2021-10-13 10:11:41 +0200

    /

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -37,43 +37,8 @@ class QuestionController extends AbstractController
 
     public function new_new(EntityManagerInterface $entityManager)
     {
-        $question = new Question();
-        $question->setName('Missing pants')
-            ->setSlug('missing-pants-'.rand(0,1000))
-            ->setQuestion(<<<EOF
- 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-Vestibulum varius felis in nulla tempus, et porttitor sem semper.
-Morbi pharetra at orci eget efficitur.
-Curabitur vitae odio interdum, mattis justo ut, efficitur mi.
-Morbi eget porta libero, eu dignissim nisl.
-Integer dapibus est sed velit scelerisque bibendum. 
-Etiam neque magna, gravida nec posuere eu, tincidunt at turpis.
-In molestie nisl at lacus porta rhoncus ac a ligula.
-Sed suscipit hendrerit augue, non vulputate orci consectetur sit amet. 
-Nulla consequat eros in lacus faucibus, quis facilisis nisl viverra. 
-Fusce justo ex, imperdiet non dolor sit amet, eleifend aliquam elit. 
-Aliquam eget porttitor dolor. Aliquam dictum ligula nunc, vel molestie tellus dapibus at.
 
-EOF
-            );
-        if (rand(1,10)>2){
-            $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
-        }
-
-        $question->setVotes(rand(-10,20));
-
-
-
-        $entityManager->persist($question);
-        $entityManager->flush();
-
-        return new Response(sprintf(
-            'Well hallo! The shiny new question is id #%d, slug %s',
-                $question->getId(),
-                $question->getSlug()
-            )
-        );
+        return new Response('Sounds like a GREAT feature for v2');
     }
 
     public function show(Question $question)
